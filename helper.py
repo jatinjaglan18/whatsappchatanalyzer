c54d51236cb8ca7ab42c1ffbbfa96fe6f2a934be
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2023-11-17 10:18:19 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,8 +1,8 @@
-#from urlextract import URLExtract
-#from wordcloud import WordCloud
+from urlextract import URLExtract
+from wordcloud import WordCloud
 import pandas as pd
 from collections import Counter
-#import emoji
+import emoji
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from scipy.special import softmax
